b509912a280a4e310cdc90b33858806fd9f8cd68
yunyan520 <lidianchao_520@163.com>
2017-07-04 10:58:59 +0800

    afn

---
+++ AFNManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNManager
+//
+//  Created by lidianchao on 2017/7/4.
+//  Copyright © 2017年 lidianchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+target ‘AFNManager’ do
+pod 'AFNetworking', '~> 3.1.0'
+end
